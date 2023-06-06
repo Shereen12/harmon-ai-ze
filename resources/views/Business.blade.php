9e8f16f309184327040f6e58a4a15d2edd53ee0c
Shereen12 <shereen12shereen@gmail.com>
2023-06-06 13:44:57 +0300

    adjust styling

--- resources/views/Business.blade.php
+++ resources/views/Business.blade.php
@@ -16,14 +16,14 @@
         </div>
 
         <div class="lg:w-1/3 w-11/12 h-max lg:h-80 border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
-            <div class="lg:relative lg:top-1/5">
+            <div class="lg:relative lg:top-1/4">
                 <a class="underline underline-offset-8 text-2xl text-white" href="https://www.neuraltext.com/?via=uniteai">Neuraltext</a>
                 <p class="mt-4 text-right font-cairo text-xl text-white">يهدف هذا الموقع إلى تغطية عملية المحتوى بالكامل ، من الفكرة إلى التنفيذ ، باستخدام الذكاء الاصطناعي ، وهو مؤلف نصوص للذكاء الاصطناعي ، وأداة تحسين محركات البحث، وأداة البحث عن الكلمات الرئيسية</p>
             </div>
         </div>
 
         <div class="lg:w-1/3 w-11/12 h-max lg:h-80 border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
-            <div class="lg:relative lg:top-1/5">
+            <div class="lg:relative lg:top-1/4">
                 <a class="underline underline-offset-8 text-2xl text-white"  href="https://speechify.com/?via=uniteai&source=fb-for-mobile">Speechify</a>
                 <p class="mt-4 text-right font-cairo text-xl text-white">يمكن لهذا الموقع تحويل النص بأي تنسيق إلى كلام طبيعي، ويمكن للنظام الأساسي أخذ الملفات أو رسائل البريد الإلكتروني أو المستندات أو المقالات وتحويلها إلى صوت يمكن الاستماع إليه بدلاً من قراءته</p>
             </div>
@@ -31,14 +31,14 @@
 
     </div>
     <div class="lg:m-4 lg:flex mb-4 lg:w-2/3">
-        <div class="lg:w-1/2 w-11/12 h-max border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
-            <div class="lg:relative lg:top-1/4">
+        <div class="lg:w-1/2 w-11/12 h-max lg:h-80 border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
+            <div class="lg:relative lg:top-[25px]">
                 <a class="underline underline-offset-8 text-2xl text-white"  href="https://www.tidio.com/">Tidio</a>
                 <p class="mt-4 text-right font-cairo text-xl text-white"> يقدم هذا الموقع حلاً مبسطًا للشركات لإضافة شات بوت إلى موقع الويب الخاص بهم، وعلى الفور، يمكنك الدردشة مع العملاء وحل مشاكلهم في الوقت الفعلي ، كما أنه يجعل من السهل تقديم امتيازات مثل الخصومات المخصصة بناءً على سجل التصفح، ويمكن للذكاء الاصطناعي أيضًا تقديم توصيات المنتج بناءً على سلوكهم</p>
             </div>
         </div>
         <div class="lg:w-1/2 w-11/12 h-max lg:h-80 border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
-            <div class="lg:relative lg:top-1/8">
+            <div class="lg:relative lg:top-1/4">
                 <a class="underline underline-offset-8 text-2xl text-white" href="https://murf.ai/">Murf</a>
                 <p class="mt-4 text-right font-cairo text-xl text-white">يمكّن هذا الموقع أي شخص من تحويل النص إلى كلام ، والتعليقات الصوتية ، والإملاءات ، ويتم استخدامه من قبل مجموعة واسعة من المحترفين مثل مطوري المنتجات ، والبودكاست ، والمعلمين ، وقادة الأعمال</p>
             </div>
