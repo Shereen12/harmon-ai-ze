95ad059df185fd80927bf54bc02c45946b66d402
Shereen12 <shereen12shereen@gmail.com>
2023-06-06 12:46:54 +0300

    remove positioning from mobile

--- resources/views/Dubbing.blade.php
+++ resources/views/Dubbing.blade.php
@@ -7,14 +7,14 @@
 
     <div class="lg:m-4 lg:flex mb-4">
         <div class="lg:w-1/2 w-11/12  lg:h-64 h-max  border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
-            <div class="relative top-1/4">
+            <div class="lg:relative lg:top-1/4">
                 <a class="underline underline-offset-8 text-white text-2xl" href="https://beta.elevenlabs.io/">Elevenlabs</a>
                 <p class="mt-4 text-xl text-right font-cairo text-white">البرنامج الأكثر واقعية لتحويل النص إلى كلام واستنساخ الصوت. يجلب هذا الموقع الأصوات الأكثر إقناعًا وثراءً وواقعية للمبدعين والناشرين الباحثين عن الأدوات المثلى لسرد القصص</p>
             </div>
         </div>
 
         <div class="lg:w-1/2 w-11/12 lg:h-64 h-max border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
-            <div class="relative top-1/4">
+            <div class="lg:relative lg:top-1/4">
                 <a class="underline underline-offset-8 text-2xl text-white" href="http://autocap.app.s3-website-us-east-1.amazonaws.com/index.html">Autocap</a>
                 <p class="mt-4 text-right font-cairo text-xl text-white">التسميات التوضيحية التلقائية للفيديو ، يستخدم هذا التطبيق أحدث تقنيات التعرف على الصوت لنسخ الصوت من مقاطع الفيديو في أسرع وقت ممكن</p>
             </div>
--- resources/views/Entertainment.blade.php
+++ resources/views/Entertainment.blade.php
@@ -7,14 +7,14 @@
 
     <div class="lg:m-4 lg:flex mb-4">
         <div class="lg:w-1/2 w-11/12  lg:h-64 h-max border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
-            <div class="relative top-1/4">
+            <div class="lg:relative lg:top-1/4">
                 <a class="underline underline-offset-8 text-white text-2xl" href="https://www.geoguessr.com/">Geogusser</a>
                 <p class="mt-4 text-xl text-right font-cairo text-white">اكتشف العالم! انزل في أي مكان من شوارع مصر المزدحمة إلى شواطئ بالي الجميلة.</p>
             </div>
         </div>
 
         <div class="lg:w-1/2 w-11/12 lg:h-64 h-max border border-gray-300 rounded-lg p-4 m-4 bg-blue-900">
-            <div class="relative top-1/4">
+            <div class="lg:relative lg:top-1/4">
                 <a class="underline underline-offset-8 text-2xl text-white" href="https://shadowart.withgoogle.com/?lang=en-us">Shadow Art</a>
                 <p class="mt-4 text-right font-cairo text-xl text-white">تعلم كيفية صنع دمى الظل بمساعدة الذكاء الاصطناعي!</p>
             </div>
