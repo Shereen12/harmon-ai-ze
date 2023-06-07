c7336eaf8a138ab20b380a2637179dbbffc74d6a
Shereen12 <shereen12shereen@gmail.com>
2023-06-07 21:04:33 +0300

    some modifications for speed

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,7 @@
 function displayCategories(parentElement, calledFrom) {
-    if (calledFrom == "nav" && $("#categories-list").length > 0) {
-        return;
-    }
     if (calledFrom == "hamburger") {
         if ($("#categories-list").length > 0) {
             hideCategories("hamburger");
-            //$("#arrow").addClass("rotate-180");
             return;
         }
     }
@@ -47,29 +43,7 @@ function displayCategories(parentElement, calledFrom) {
 
     $("#arrow").addClass("rotate-180");
 
-    if (calledFrom == "nav") {
-        addCategoriesListForDesktop(parentElement, categories);
-    } else {
-        addCategoriesListForMobile(parentElement, categories);
-    }
-}
-
-function addCategoriesListForDesktop(parentElement, data) {
-    $("#" + parentElement).append(`
-                <div id="categories-list" class="z-10 bg-blue-900 absolute top p-2 rounded">
-                    <ul id="categories-content" class="text-white"></ul>
-                </div>
-            `);
-    for (let i = 0; i < data.length; i++) {
-        if (i < data.length - 1)
-            $("#categories-content").append(`
-                        <li class="border-b b-white hover:text-blue-900 hover:bg-white hover:rounded p-2"><a href="/${data[i].slug}">${data[i].name}</a></li>
-                    `);
-        else
-            $("#categories-content").append(`
-                <li class="border-white hover:text-blue-900 hover:bg-white hover:rounded p-2"><a href="/${data[i].slug}">${data[i].name}</a></li>
-            `);
-    }
+    addCategoriesListForMobile(parentElement, categories);
 }
 
 function addCategoriesListForMobile(parentElement, data) {
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="h-max border rounded-lg bg-white mx-auto grid grid-cols-1">
-    <img src="/pictures/main.png"/>
+    <img src="/pictures/main.png" class="w-full h-full"/>
     <p class="leading-loose text-blue-900 font-cairo relative m-auto p-4 inline-block align-middle text-right text-3xl">
         الذكاء الاصطناعي بقى دلوقتي جزء لا يتجزأ من حياتنا، في مننا ناس بقت معتمدة عليه و دة الشئ ال بيزيد من خطورته علينا و في ناس بتتجاهل وجوده عشان تتجنب أضراره؛ وعشان احنا عارفين قد ايه الموضوع يلخبط، موقعنا اتعمل عشان يساعدك تعرف اكتر عن تقنيات الذكاء الإصطناعي و ازاي تستغلها لصالحك مع تجنب أضرارها. هنا هتقدر تلاقي مواقع و أدوات متعددة و مختلفة بتعمل بالذكاء الإصطناعي و تقدر تساعدك في حاجات ومجالات كتير زي الدراسة، التصميم، البرمجة و الكتابة وكمان الترفيه وغيرهم كتير من المجالات
     </p>
